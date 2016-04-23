624bec07dec3888e32546f754f6b8385d16b0b7c
Jordan Kasper <jordan@jordankasper.com>
2016-04-23 17:16:35 +0100

    Forgot to switch out values in datasources for prod

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,11 @@
-{
-  "temp": {
-    "name": "temp",
-    "connector": "memory"
-  },
-  "db": {
-    "name": "db",
-    "connector": "memory",
-    "file": "data.json"
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'mongodb',
+    'host': 'ds023398.mlab.com',
+    'port': 23398,
+    'database': 'tiy-blog',
+    'username': 'tiy-blog-api',
+    'password': process.env.TIY_BLOG_PASS || ''
   }
-}
+};
